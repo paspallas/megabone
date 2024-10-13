aefb26e3e464c20802eca0f7a8d6cbf480dc4094
Pablo Porta <paspallasdev@gmail.com>
2024-10-13 23:00:04 +0200

    add qtmodern styles

--- src/megabone/entry.py
+++ src/megabone/entry.py
@@ -2,6 +2,7 @@ import sys
 
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication
+from qtmodern import styles
 from typing import List
 
 from .ui import MegaBoneMainWindow
@@ -10,6 +11,7 @@ from .ui import MegaBoneMainWindow
 def entry(args: List[str]) -> None:
     QApplication.setAttribute(Qt.AA_EnableHighDpiScaling, True)
     app = QApplication(sys.argv)
+    styles.dark(app)
     
     window = MegaBoneMainWindow()
     desktop = QApplication.desktop().screenGeometry(0)
